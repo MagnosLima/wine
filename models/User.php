87d22bec51db7c9377c65f2c08dcc52dd87902d6
Magnos Lima <magandroid6.1@gmail.com>
2018-10-02 15:05:32 -0300

    programado o beforeSave para criptografar a senha de usuário ok agora

---
+++ models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $access_token
+ * @property string $auth_key
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    public function beforeSave($insert)
+    {
+        //Ação disparada no insert ou update.
+        //Fazer a mágica aqui - atributos sujos (valores alterados).
+        Yii::trace("Antes de criptografar");
+        if (array_key_exists('password', $this->dirtyAttributes)) {
+            $this->password = Yii:: $app->getSecurity()->generatePasswordHash($this->password);
+            Yii::trace("Depois de criptografar");
+        }
+
+        return parent::beforeSave($insert);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'required'],
+            [['username'], 'string', 'max' => 45],
+            [['password', 'access_token', 'auth_key'], 'string', 'max' => 100],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'password' => Yii::t('app', 'Password'),
+            'access_token' => Yii::t('app', 'Access Token'),
+            'auth_key' => Yii::t('app', 'Auth Key'),
+        ];
+    }
+
+    public function validatePassword($password){
+        return Yii::$app->getSecurity()->validatePassword(password, $this->password);
+    }
+}
