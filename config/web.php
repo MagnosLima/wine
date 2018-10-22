c64adc82178749209e78d90b69257e026541907d
Magnos Lima <magandroid6.1@gmail.com>
2018-10-22 17:07:32 -0300

    configurando rbac para db em web.php e console.php

--- config/web.php
+++ config/web.php
@@ -46,6 +46,9 @@ $config = [
                 ],
             ],
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'db' => $db,
         /*
         'urlManager' => [
